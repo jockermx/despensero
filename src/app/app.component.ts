7a6d21204db44847bc930ede1ffd9f4af479e493
jocker <antonio.vsalas@gmail.com>
2019-02-11 16:16:35 -0600

    se agregan avances de search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private qrscanner: QRScanner,
+    private qrscannerStatus: QRScannerStatus
   ) {
     this.initializeApp();
   }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page implements OnInit {
+  products: any = [];
+  productsList: any = [];
+  constructor(
+    public productService: ProductService
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.productService.getProducts().valueChanges()
+    .subscribe( (productsFB) => {
+      //console.log(productsFB);
+      this.products = productsFB;
+    });
+  }
+
+  getItems(searchbar) {
+    // set q to the value of the searchbar
+    const request = searchbar.srcElement.value;
+
+    // if the value is an empty string don't filter the items
+    if (!request) {
+      this.productsList = this.products;
+      return;
+    }
+
+    this.productsList = this.products.filter((v) => {
+      if (v.name && request) {
+        if (v.name.toLowerCase().indexOf(request.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      }
+    });
+
+  console.log(request, this.productsList.length);
+
+  }
+
+}
